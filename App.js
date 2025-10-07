2f6f16d43499a890b053fed9df5b9841dc89219a
CalvaryBloom <borjap99@gmail.com>
2025-10-07 18:36:41 +0200

    Ejercicio 4

--- App.js
+++ App.js
@@ -3,75 +3,60 @@
 * AUTOR: BORJA PARDO JUANES
 * FECHA: 07/10/2025 
 *
-* Apartado 3:
-* Sin emplear arrays en los estados y utilizando una única función para manejar 
-* los dos eventos, implementa las instrucciones necesarias para que cuando el usuario 
-* pulse sobre el cuadrado superior, los dos cuadrados sean de color verde y se aumente su 
-* tamaño –de ambos-. Si el usuario pulsa sobre el cuadrado superior, ambos serán de color 
-* amarillo y disminuirán su tamaño.
+* Apartado 4:
+* Implementa las instrucciones necesarias para renderizar en la interfaz gráfica 
+* un componente TextInput que recoja el contenido que introduzca el usuario, 
+* debajo, un componente Pressable. Implementa la lógica necesaria para que 
+* cuando el usuario pulse el botón de Pressable, salte una alerta indicando 
+* los siguientes casos: si se ha introducido texto, si se ha introducido un número o si no se ha introducido nada.
 */
-import { Text, Pressable, Image, View, StyleSheet } from 'react-native';
+import { Text, Pressable, Image, View, StyleSheet, TextInput } from 'react-native';
 import { useState } from 'react';
 
 export default function App() {
 
-  const [square, setSquare] = useState({
-    color: 'green',
-    width: 150,
-    height: 150,
-  });
+  const [text, setText] = useState('');
 
   function handleOnPress(number) {
-    if (number === 0) {
-      setSquare({
-        color: 'green',
-        width: square.width + 10,
-        height: square.height + 10
-      })
-    } else {
-      setSquare({
-        color: 'yellow',
-        width: square.width - 10,
-        height: square.height - 10
-      })
+    if (isNaN(text)) {
+      setText('');
+      alert('Has introducido texto');
+    } else if (text === '') {
+      alert('No has introducido nada');
+    } else if (!isNaN(text)) {
+      alert('Has introducido un número');
+      setText('');
     }
   }
 
   return (
     <View style={styles.container}>
-      <Pressable
-        onPress={() => handleOnPress(0)}
-        style={[
-          styles.square,
-          {
-            width: square.width,
-            height: square.height,
-            backgroundColor: square.color
-          },
-        ]}
+      <TextInput 
+        style={{ height: 40 }}
+        placeholder="Inserta tu texto..."
+        onChangeText={(newText) => setText(newText)}
+        defaultValue={text}
       />
-      <Pressable
-        onPress={() => handleOnPress(1)}
-        style={[
-          styles.square,
-          {
-            width: square.width,
-            height: square.height,
-            backgroundColor: square.color
-          }
-        ]}
-      />
+      <Text style={{ padding: 10, fontSize: 42 }}>{text}</Text>   
+      <Pressable onPress={() => handleOnPress()}>
+        <Text style={styles.text}>Pulsame</Text>
+      </Pressable>   
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, backgroundColor: 'white', alignItems: 'center', justifyContent: 'center',
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  square: {
-    marginTop: -6,
-    width: 200,
-    height: 200, backgroundColor: 'yellow',
+  text: {
+    height: 40,
+    width: 80,
+    backgroundColor: 'lightblue',
+    borderRadius: 8,
+    padding: 6,
   },
 });
