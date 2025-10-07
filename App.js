2831c24bba73a5202b659fbe021f20840c408170
CalvaryBloom <borjap99@gmail.com>
2025-10-07 13:29:52 +0200

    Ejercicio 1 corregido

--- App.js
+++ App.js
@@ -19,42 +19,38 @@ export default function App() {
   const [text3, setText3] = useState('Texto');
   const [text4, setText4] = useState('Texto');
 
-  function handleOnPress(opcion){
-  if (opcion === 1)
-    setText1(text1 === 'Texto' ? 'Texto cambiado' : 'Texto');
-  
-  if (opcion === 2)
-    setText2(text2 === 'Texto' ? 'Texto cambiado' : 'Texto');
-  
-  if (opcion === 3)
-    setText3(text3 === 'Texto' ? 'Texto cambiado' : 'Texto');
-  
-  if (opcion === 4)
-    setText4(text4 === 'Texto' ? 'Texto cambiado' : 'Texto');
-}
+  function handleOnPress(opcion) {
+    if (opcion === 1)
+      setText1(text1 === 'Texto' ? 'Texto cambiado' : 'Texto');
+
+    if (opcion === 2)
+      setText2(text2 === 'Texto' ? 'Texto cambiado' : 'Texto');
+
+    if (opcion === 3)
+      setText3(text3 === 'Texto' ? 'Texto cambiado' : 'Texto');
+
+    if (opcion === 4)
+      setText4(text4 === 'Texto' ? 'Texto cambiado' : 'Texto');
+  }
 
   return (
     <View style={styles.container}>
-      <Pressable onPress={() => handleOnPress(1)}>
-        <Text style={styles.text}>
-          {text1}
-        </Text>
-      </Pressable>
-      <Pressable onPress={() => handleOnPress(2)}>
-        <Text style={styles.text}>
-          {text2}
-        </Text>
-      </Pressable>
-      <Pressable onPress={() => handleOnPress(3)}>
-        <Text style={styles.text}>
-          {text3}
-        </Text>
-      </Pressable>
-      <Pressable onPress={() => handleOnPress(4)}>
-        <Text style={styles.text}>
-          {text4}
-        </Text>
-      </Pressable>
+      <Text style={styles.text} onPress={() => handleOnPress(1)}>
+        {text1}
+      </Text>
+
+      <Text style={styles.text} onPress={() => handleOnPress(2)}>
+        {text2}
+      </Text>
+
+      <Text style={styles.text} onPress={() => handleOnPress(3)}>
+        {text3}
+      </Text>
+
+      <Text style={styles.text} onPress={() => handleOnPress(4)}>
+        {text4}
+      </Text>
+
     </View>
   );
 }
